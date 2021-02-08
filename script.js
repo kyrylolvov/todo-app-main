ee32763e9bad1dc4a39be660a3ccfb631a63dc52
Kyrylo Lvov <70720713+kyrylolvov@users.noreply.github.com>
2021-02-07 23:12:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 "use strict";
 
 const inputTag = document.querySelector(".input-tag");
-const containerMovements = document.querySelector(".todo-movements");
+const todos = document.querySelector(".todo-movements");
 let deleteButton = document.querySelectorAll(".movements-row-cross");
 
 inputTag.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     if (inputTag.value) {
       const html = `
-      <div class="todo-movements-row">
+      <div class="todo-movements-row todo-incomplete">
       <div class="todo-movement-row-left">
         <div class="movements-row-check input-circle"></div>
         <p class="movements-row-text">${inputTag.value}</p>
@@ -19,16 +19,14 @@ inputTag.addEventListener("keypress", (e) => {
         class="movements-row-cross"
       />
     </div>`;
-      containerMovements.insertAdjacentHTML("afterbegin", html);
-      deleteButton = document.querySelectorAll(".movements-row-cross");
+      todos.insertAdjacentHTML("afterbegin", html);
       inputTag.value = "";
     }
   }
 });
 
-deleteButton = document.querySelectorAll(".movements-row-cross");
-deleteButton.forEach((button) => {
-  button.addEventListener("click", function () {
-    console.log(`Hello there ${button}`);
-  });
+todos.addEventListener("click", function (e) {
+  if (e.target.classList.contains("movements-row-cross")) {
+    e.target.parentNode.remove();
+  }
 });
