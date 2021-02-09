78e340ea8c9a6a6114927bd93c38f386f8a88011
Kyrylo Lvov <70720713+kyrylolvov@users.noreply.github.com>
2021-02-08 23:08:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 "use strict";
 
 const inputTag = document.querySelector(".input-tag");
-const todos = document.querySelector(".todo-movements");
+const todos = document.querySelector(".todo-container");
 const todosLeft = document.querySelector(".todo-number");
 
 function todosCountRefresh() {
   todosLeft.innerHTML = document.querySelectorAll(".todo-incomplete").length;
+  if (document.querySelectorAll(".todo-incomplete").length === 6) {
+    alert("Too many tasks!");
+    document.querySelectorAll(".todo-movements-row")[0].remove();
+  }
 }
 
 todosCountRefresh();
@@ -17,7 +21,8 @@ inputTag.addEventListener("keypress", (e) => {
       const html = `
       <div class="todo-movements-row todo-incomplete">
       <div class="todo-movement-row-left">
-        <div class="movements-row-check input-circle"></div>
+        <div class="movements-row-check input-circle">
+        </div>
         <p class="movements-row-text">${inputTag.value}</p>
       </div>
       <img
@@ -28,23 +33,22 @@ inputTag.addEventListener("keypress", (e) => {
     </div>`;
       todos.insertAdjacentHTML("afterbegin", html);
       inputTag.value = "";
-//   Refresh todo count
       todosCountRefresh();
     }
   }
 });
 
 todos.addEventListener("click", function (e) {
-  if (e.target.classList.contains("todo-movements-row")) {
+  if (e.target.classList.contains("movements-row-check")) {
     if (e.target.parentNode.parentNode.classList.contains("todo-completed")) {
-      e.target.parentNode.parentNode.classList.remove("todo-completed");
       e.target.parentNode.parentNode.classList.add("todo-incomplete");
+      e.target.parentNode.parentNode.classList.remove("todo-completed");
       // refresh the items left
       todosCountRefresh();
     } else if (
       e.target.parentNode.parentNode.classList.contains("todo-incomplete")
     ) {
-      e.target.parentNode.parentNode.classList.add("todo-complete");
+      e.target.parentNode.parentNode.classList.add("todo-completed");
       e.target.parentNode.parentNode.classList.remove("todo-incomplete");
       // refresh the items left
       todosCountRefresh();
@@ -55,4 +59,35 @@ todos.addEventListener("click", function (e) {
     e.target.parentNode.remove();
   }
   todosCountRefresh();
+
+  // Clear all completed
+  document.querySelector(".todo-action").onclick = function () {
+    document.querySelectorAll(".todo-completed").forEach((completed) => {
+      completed.remove();
+    });
+  };
+
+  document.querySelector("#sort-active").onclick = function () {
+    document.querySelectorAll(".todo-completed").forEach((completed) => {
+      completed.classList.add("todo-hidden");
+    });
+    document.querySelectorAll(".todo-incomplete").forEach((completed) => {
+      completed.classList.remove("todo-hidden");
+    });
+  };
+
+  document.querySelector("#sort-completed").onclick = function () {
+    document.querySelectorAll(".todo-incomplete").forEach((completed) => {
+      completed.classList.add("todo-hidden");
+    });
+    document.querySelectorAll(".todo-completed").forEach((completed) => {
+      completed.classList.remove("todo-hidden");
+    });
+  };
+
+  document.querySelector("#sort-all").onclick = function () {
+    document.querySelectorAll(".todo-movements-row").forEach((completed) => {
+      completed.classList.remove("todo-hidden");
+    });
+  };
 });
